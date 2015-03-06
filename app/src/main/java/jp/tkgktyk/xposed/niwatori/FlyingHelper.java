a21dea4a0ebd71f2f23e057788e43db59ca07d6b
tkgktyk <tkgktyk@gmail.com>
2015-03-07 03:28:31 +0900

    Replaced from injection FlyingLayout to native mod

--- app/src/main/java/jp/tkgktyk/xposed/niwatori/FlyingHelper.java
+++ app/src/main/java/jp/tkgktyk/xposed/niwatori/FlyingHelper.java
@@ -1,15 +1,21 @@
 package jp.tkgktyk.xposed.niwatori;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.Nullable;
+import android.os.Build;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
@@ -18,124 +24,75 @@ import jp.tkgktyk.flyinglayout.FlyingLayout;
 /**
  * Created by tkgktyk on 2015/02/13.
  */
-public class FlyingHelper extends XposedModule {
-    private static final String FIELD_FLYING_HELPER = NFW.NAME + "_flyingHelper";
+public class FlyingHelper  extends FlyingLayout.Helper {
+    private final NFW.Settings mSettings;
+    private final Rect mChildRect = new Rect();
+    private final Method mGetPaddingLeftWithForeground;
+    private final Method mGetPaddingTopWithForeground;
+    private final Method mGetPaddingRightWithForeground;
+    private final Method mGetPaddingBottomWithForeground;
 
-    private NFW.Settings mSettings;
-
-    private ViewGroup mTargetView;
-    private InputMethodManager mInputMethodManager;
-
-    private FlyingLayout mFlyingLayout;
-    private FrameLayout mContainer;
-
+    private final InputMethodManager mInputMethodManager;
     private Drawable mBoundaryDrawable;
+    private boolean mBoundaryShown = false;
 
-    public FlyingHelper(NFW.Settings settings) {
+    public FlyingHelper(ViewGroup view, int frameLayoutHierarchy, NFW.Settings settings)
+            throws NoSuchMethodException {
+        super(view);
         mSettings = settings;
-    }
 
-    @Nullable
-    public static View getHelperHolder(View target) {
-        if (target != null) {
-            return target.getRootView();
-        }
-        return null;
-    }
+        mGetPaddingLeftWithForeground = getPaddingMethod(view, frameLayoutHierarchy, "getPaddingLeftWithForeground");
+        mGetPaddingTopWithForeground =  getPaddingMethod(view, frameLayoutHierarchy, "getPaddingTopWithForeground");
+        mGetPaddingRightWithForeground =  getPaddingMethod(view, frameLayoutHierarchy, "getPaddingRightWithForeground");
+        mGetPaddingBottomWithForeground =  getPaddingMethod(view, frameLayoutHierarchy, "getPaddingBottomWithForeground");
 
-    @Nullable
-    public static FlyingHelper getFromHelperHolder(View holder) {
-        if (holder != null) {
-            return (FlyingHelper) XposedHelpers.getAdditionalInstanceField(
-                    holder, FIELD_FLYING_HELPER);
-        }
-        return null;
-    }
-
-    @Nullable
-    public static FlyingHelper getFrom(View target) {
-        View holder = getHelperHolder(target);
-        return getFromHelperHolder(holder);
-    }
-
-    public NFW.Settings getSettings() {
-        return mSettings;
-    }
-
-//    /**
-//     * Before:
-//     * parent
-//     * - child
-//     * - ...
-//     * - target
-//     * - ...
-//     * <p/>
-//     * After:
-//     * parent
-//     * - child
-//     * - ...
-//     * - FlyingLayout
-//     * -- Container
-//     * --- target
-//     * - ...
-//     *
-//     * @param parent
-//     * @param targetIndex
-//     * @return
-//     * @throws Throwable
-//     */
-//    public View install(ViewGroup parent, int targetIndex) throws Throwable {
-//        final View target = parent.getChildAt(targetIndex);
-//        mTargetView = target;
-//        parent.removeView(target);
-//        mInputMethodManager = (InputMethodManager) target.getContext()
-//                .getSystemService(Context.INPUT_METHOD_SERVICE);
-//
-//        final Context context = target.getContext();
-//        prepareBoundary(context);
-//        installFlyingLayout(context);
-//
-//        // wrap with FlyingLayout
-//        mContainer.addView(target);
-//        parent.addView(mFlyingLayout, targetIndex);
-//
-//        final View holder = getHelperHolder(target);
-//        XposedHelpers.setAdditionalInstanceField(holder, FIELD_FLYING_HELPER, this);
-//
-//        return holder;
-//    }
-
-    /**
-     * Before:
-     * target
-     * - contents
-     * <p/>
-     * After:
-     * target
-     * - FlyingLayout
-     * -- Container
-     * --- contents
-     *
-     * @param target
-     * @return
-     * @throws Throwable
-     */
-    public View install(ViewGroup target) throws Throwable {
-        mTargetView = target;
-        mInputMethodManager = (InputMethodManager) target.getContext()
+        mInputMethodManager = (InputMethodManager) view.getContext()
                 .getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        final Context context = target.getContext();
-        prepareBoundary(context);
-        installFlyingLayout(context);
-
-        return attachTo(target);
+        reinitialize();
     }
 
-    private void prepareBoundary(Context context) {
+    private Method getPaddingMethod(Object object, int hier, String methodName)
+            throws NoSuchMethodException{
+        Class<?> cls = object.getClass();
+        for (int i = 0; i < hier; ++i) {
+            cls = cls.getSuperclass();
+        }
+        final Method method = cls.getDeclaredMethod(methodName);
+        if (!method.isAccessible()) {
+            AccessController.doPrivileged(new PrivilegedAction<Object>() {
+                public Object run() {
+                    method.setAccessible(true);
+                    return null;
+                }
+            });
+        }
+        return method;
+    }
+
+    private void reinitialize() {
+        setSpeed(mSettings.speed);
+        setTouchEventEnabled(false);
+        setUseContainer(true);
+        setOnFlyingEventListener(new FlyingLayout.SimpleOnFlyingEventListener() {
+            @Override
+            public void onClickOutside(ViewGroup v) {
+                resetState();
+            }
+
+            @Override
+            public void onDoubleClickOutside(ViewGroup v) {
+                pin();
+            }
+        });
+
+        prepareBoundary();
+    }
+
+    private void prepareBoundary() {
         Context flyContext = null;
         try {
-            flyContext = context.createPackageContext(
+            flyContext = getAttachedView().getContext().createPackageContext(
                     NFW.PACKAGE_NAME, Context.CONTEXT_IGNORE_SECURITY);
         } catch (Throwable t) {
             XposedBridge.log(t);
@@ -145,116 +102,22 @@ public class FlyingHelper extends XposedModule {
         }
     }
 
-    private void installFlyingLayout(Context context) throws Throwable {
-        mFlyingLayout = new FlyingLayout(context);
-        mContainer = new FrameLayout(context);
-        // setup view hierarchy
-        mFlyingLayout.addView(mContainer);
-
-        // setup FlyingLayout
-        final FlyingLayout.Helper helper = mFlyingLayout.getHelper();
-        helper.setSpeed(mSettings.speed);
-        final Context flyContext = context.createPackageContext(
-                NFW.PACKAGE_NAME, Context.CONTEXT_IGNORE_SECURITY);
-        final int padding = flyContext.getResources()
-                .getDimensionPixelSize(R.dimen.flying_view_padding);
-        helper.setHorizontalPadding(padding);
-        helper.setVerticalPadding(padding);
-        helper.setTouchEventEnabled(false);
-        helper.setUseContainer(true);
-        helper.setOnFlyingEventListener(new FlyingLayout.SimpleOnFlyingEventListener() {
-            @Override
-            public void onClickOutside(ViewGroup v) {
-                resetState();
-            }
-
-            @Override
-            public void onLongPressOutside(ViewGroup v) {
-                // nothing to do
-            }
-
-            @Override
-            public void onDoubleClickOutside(ViewGroup v) {
-                pin();
-            }
-        });
-    }
-
-    /**
-     * Before:
-     * target
-     * - contents
-     * <p/>
-     * After:
-     * target
-     * - FlyingLayout
-     * -- Container
-     * --- contents
-     *
-     * @param target
-     */
-    private View attachTo(ViewGroup target) {
-        final List<View> contents = new ArrayList<>();
-        for (int i = 0; i < target.getChildCount(); ++i) {
-            contents.add(target.getChildAt(i));
-        }
-        target.removeAllViews();
-        for (View v : contents) {
-            mContainer.addView(v, v.getLayoutParams());
-        }
-        target.addView(mFlyingLayout);
-        final View holder = getHelperHolder(target);
-        XposedHelpers.setAdditionalInstanceField(holder, FIELD_FLYING_HELPER, this);
-
-        return holder;
-    }
-
-    public void uninstall() throws Throwable {
-        if (mTargetView == null) {
-            log("not installed");
-            return;
-        }
-        final View holder = getHelperHolder(mTargetView);
-        XposedHelpers.removeAdditionalInstanceField(holder, FIELD_FLYING_HELPER);
-
-        mTargetView.removeView(mFlyingLayout);
-
-        final List<View> contents = new ArrayList<>();
-        for (int i = 0; i < mContainer.getChildCount(); ++i) {
-            contents.add(mContainer.getChildAt(i));
-        }
-        mContainer.removeAllViews();
-        for (View v : contents) {
-            mTargetView.addView(v, v.getLayoutParams());
-        }
-    }
-
-
-    public FlyingLayout getFlyingLayout() {
-        return mFlyingLayout;
-    }
-
     private void setBoundaryShown(boolean shown) {
-        if (mSettings.notifyFlying) {
-            if (shown) {
-                mContainer.setForeground(mBoundaryDrawable);
-            } else {
-                mContainer.setForeground(null);
-            }
-        }
+        mBoundaryShown = shown;
+        getAttachedView().invalidate();
     }
 
     public boolean isFlying() {
-        return mFlyingLayout.getHelper().getTouchEventEnabled();
+        return getTouchEventEnabled();
     }
 
     private void disableFlying() {
-        mFlyingLayout.getHelper().setTouchEventEnabled(false);
+        setTouchEventEnabled(false);
         setBoundaryShown(false);
     }
 
     private void enableFlying() {
-        mFlyingLayout.getHelper().setTouchEventEnabled(true);
+        setTouchEventEnabled(true);
         setBoundaryShown(true);
     }
 
@@ -273,9 +136,9 @@ public class FlyingHelper extends XposedModule {
     private void toggle() {
         if (isFlying()) {
             disableFlying();
-            mFlyingLayout.getHelper().goHome(mSettings.animation);
+            goHome(mSettings.animation);
         } else {
-            if (mFlyingLayout.getHelper().staysHome()) {
+            if (staysHome()) {
                 moveToInitialPosition(false);
                 hideSoftInputMethod();
             }
@@ -284,7 +147,7 @@ public class FlyingHelper extends XposedModule {
     }
 
     private void pin() {
-        if (mFlyingLayout.getHelper().staysHome()) {
+        if (staysHome()) {
             moveToInitialPosition(true);
             hideSoftInputMethod();
             disableFlying();
@@ -297,11 +160,11 @@ public class FlyingHelper extends XposedModule {
     }
 
     private void pinOrReset() {
-        if (mFlyingLayout.getHelper().staysHome()) {
+        if (staysHome()) {
             moveToInitialPosition(true);
             hideSoftInputMethod();
         } else {
-            mFlyingLayout.getHelper().goHome(mSettings.animation);
+            goHome(mSettings.animation);
         }
         disableFlying();
     }
@@ -313,26 +176,119 @@ public class FlyingHelper extends XposedModule {
             pos.setXp(0);
             pos.setYp(50);
         }
-        final int x = pos.getX(mFlyingLayout);
-        final int y = pos.getY(mFlyingLayout);
+        final int x = pos.getX(getAttachedView());
+        final int y = pos.getY(getAttachedView());
         boolean moved = false;
         if (x != 0 || y != 0) {
             moved = true;
-            mFlyingLayout.getHelper().moveWithoutSpeed(x, y, mSettings.animation);
+            moveWithoutSpeed(x, y, mSettings.animation);
         }
         return moved;
     }
 
     public void resetState() {
-        if (isFlying() || !mFlyingLayout.getHelper().staysHome()) {
+        if (isFlying() || !staysHome()) {
             disableFlying();
             // goHome must be placed after pin() for "Reset when collapsed"
             // option.
-            mFlyingLayout.getHelper().goHome(mSettings.animation);
+            goHome(mSettings.animation);
         }
     }
 
     private void hideSoftInputMethod() {
-        mInputMethodManager.hideSoftInputFromWindow(mTargetView.getWindowToken(), 0);
+        mInputMethodManager.hideSoftInputFromWindow(getAttachedView().getWindowToken(), 0);
     }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        final boolean forceLeftGravity = false;
+        final ViewGroup view = getAttachedView();
+        final Rect boundaryRect = getBoundaryRect();
+
+        final int count = view.getChildCount();
+
+        int parentLeft, parentRight, parentTop, parentBottom;
+        try {
+            parentLeft = (Integer) mGetPaddingLeftWithForeground.invoke(view);
+            parentRight = right - left - (Integer) mGetPaddingRightWithForeground.invoke(view);
+
+            parentTop = (Integer) mGetPaddingTopWithForeground.invoke(view);
+            parentBottom = bottom - top - (Integer) mGetPaddingBottomWithForeground.invoke(view);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
+
+        XposedHelpers.setBooleanField(view, "mForegroundBoundsChanged", true);
+
+        boundaryRect.setEmpty();
+        for (int i = 0; i < count; i++) {
+            final View child = view.getChildAt(i);
+            if (child.getVisibility() != View.GONE) {
+                final FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) child.getLayoutParams();
+
+                final int width = child.getMeasuredWidth();
+                final int height = child.getMeasuredHeight();
+
+                int childLeft;
+                int childTop;
+
+                int gravity = lp.gravity;
+                if (gravity == -1) {
+                    gravity = DEFAULT_CHILD_GRAVITY;
+                }
+
+                final int layoutDirection =
+                        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) ?
+                                view.getLayoutDirection() : 0;
+                final int absoluteGravity = Gravity.getAbsoluteGravity(gravity, layoutDirection);
+                final int verticalGravity = gravity & Gravity.VERTICAL_GRAVITY_MASK;
+
+                switch (absoluteGravity & Gravity.HORIZONTAL_GRAVITY_MASK) {
+                    case Gravity.CENTER_HORIZONTAL:
+                        childLeft = parentLeft + (parentRight - parentLeft - width) / 2
+                                + lp.leftMargin - lp.rightMargin;
+                        break;
+                    case Gravity.RIGHT:
+                        if (!forceLeftGravity) {
+                            childLeft = parentRight - width - lp.rightMargin;
+                            break;
+                        }
+                    case Gravity.LEFT:
+                    default:
+                        childLeft = parentLeft + lp.leftMargin;
+                }
+
+                switch (verticalGravity) {
+                    case Gravity.TOP:
+                        childTop = parentTop + lp.topMargin;
+                        break;
+                    case Gravity.CENTER_VERTICAL:
+                        childTop = parentTop + (parentBottom - parentTop - height) / 2
+                                + lp.topMargin - lp.bottomMargin;
+                        break;
+                    case Gravity.BOTTOM:
+                        childTop = parentBottom - height - lp.bottomMargin;
+                        break;
+                    default:
+                        childTop = parentTop + lp.topMargin;
+                }
+
+                mChildRect.set(childLeft, childTop, childLeft + width, childTop + height);
+                if (!getUseContainer() || i == 0) {
+                    mChildRect.offset(getOffsetX(), getOffsetY());
+                    boundaryRect.union(mChildRect);
+                }
+                child.layout(mChildRect.left, mChildRect.top, mChildRect.right, mChildRect.bottom);
+            }
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        if (mBoundaryShown) {
+            mBoundaryDrawable.setBounds(getBoundaryRect());
+            mBoundaryDrawable.draw(canvas);
+        }
+    }
+
 }
